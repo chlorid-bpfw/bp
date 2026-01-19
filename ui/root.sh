4add11e9105f21e4184fba6c022909efcacaff16
OptimiDev <optimi@destable.hu>
2026-01-19 16:40:26 +0100

    Fix typo in case options and export statement

--- ui/root.sh
+++ ui/root.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-case $(gum choose "Artisan" "Extensions" "Exit" --header="blueprint-tui v$BLUEPRINT_TUI_VERSION") in
+case $(gum choose "Artisan" "Extensions" "Managment" "Exit" --header="BP v$BLUEPRINT_TUI_VERSION") in
   Artisan)
     export window="artisan-root"
   ;;
   Extensions)
     export window="extensions-root"
   ;;
+  Managment)
+    export window ="managment-root"
 esac
